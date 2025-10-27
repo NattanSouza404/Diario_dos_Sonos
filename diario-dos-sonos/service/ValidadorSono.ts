0edf88b2f7f0dd4bcb37c3beba593ca23732c567
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-27 20:43:01 -0300

    feat e test: adição de validação da duração do sono

--- diario-dos-sonos/service/ValidadorSono.ts
+++ diario-dos-sonos/service/ValidadorSono.ts
@@ -1,7 +1,14 @@
 import IntervaloSono from "@/models/IntervaloSono";
+import { diferencaEntreDatas } from "./Formatador";
 
 export const validarSono = (intervalo: IntervaloSono): void => {
     if (intervalo.horaFim <= intervalo.horaInicio) {
         throw new Error("A hora de fim deve ser maior que a hora de início.");
+    }
+
+    const duracao = diferencaEntreDatas(intervalo.horaInicio, intervalo.horaFim);
+
+    if (duracao.horas < 2 || duracao.horas > 14){
+        throw new Error("A duração do sono deve estar entre 2h e 14h.")
     }
 }
